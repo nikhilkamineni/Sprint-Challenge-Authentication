2699cca413555dac8ca943a15b679d8ea3fdde5b
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-23 11:35:36 -0400

    Finished User controller; new user POST functionality working

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,8 +5,22 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  if (!username || !password) {
+    res
+      .status(422)
+      .json({ message: 'You need to provide a username and password' });
+  }
+  const newUser = new User({ username, password });
+  newUser
+    .save()
+    .then(savedUser => {
+      res.status(200).json(savedUser);
+    })
+    .catch(err => {
+      res.status(422).json({ message: 'Error saving user to DB', error: err });
+    });
 };
 
 module.exports = {
-  createUser
+  createUser,
 };
