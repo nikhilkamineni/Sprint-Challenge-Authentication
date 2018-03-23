ed9add40eda78c332692e75bbefb62dcead4afed
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-03-23 14:15:56 -0400

    Added some basic inline styles for Jokes component

--- client/src/components/jokes.js
+++ client/src/components/jokes.js
@@ -2,17 +2,42 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getJokes } from '../actions';
 
+const styles = {
+  width: '80%'
+}
+
+const jokeStyles = {
+  border: '1px solid black',
+  padding: '5px',
+  margin: '5px',
+  listStyleType: 'none',
+};
+
+const jokeSetupStyles = {
+  fontWeight: 'bold'
+}
+
+const jokeTypeStyles = {
+  fontSize: '10px'
+}
+
 class Jokes extends Component {
   componentDidMount() {
     this.props.getJokes();
-    console.log(this.props)
+    console.log(this.props);
   }
 
   render() {
     return (
-      <ul>
+      <ul style={styles}>
         {this.props.jokes.map((joke, i) => {
-          return <li key={i}>{joke.setup}</li>;
+          return (
+            <ul key={i} style={jokeStyles}>
+              <li className="Joke-Setup" style={jokeSetupStyles}>{joke.setup}</li>
+              <li className='Joke-Punchline'>{joke.punchline}</li>
+              <li className='Joke-Type' style={jokeTypeStyles}>{joke.type}</li>
+            </ul>
+          );
         })}
       </ul>
     );
@@ -21,7 +46,7 @@ class Jokes extends Component {
 
 const mapStateToProps = state => {
   return {
-    jokes: state.jokes
+    jokes: state.jokes,
   };
 };
 
